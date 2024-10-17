5f42c0992d9ebced20040c31a4fffbdfdb3ed58f
arrivaldo <erickbarcena00@hotmail.com>
2024-10-17 03:13:18 -0600

    Initial2

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -7,31 +7,20 @@ export default function PrivacyPolicy() {
                 regarding the collection, use, and disclosure of personal information when you use our website and
                 services. Please read this policy carefully to understand how we handle your data.
             </p>
-            <div className="space-y-2">
-                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">1. Information We Collect</h2>
-                <p>
-                    We collect and use limited information to improve the user experience. The only data we collect is
-                    through Google Analytics, which includes: Usage Information: We may collect information about your
-                    interaction with our website, such as the pages you visit, your IP address, browser type, device
-                    type, and referral URLs. This information helps us understand how users interact with our website to
-                    enhance its functionality and content.
-                </p>
-            </div>
+           
 
             <div className="space-y-2">
                 <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
-                    2. How We Use Your Information
+                    1. How We Use Your Information
                 </h2>
                 <p>
-                    We use the information collected through Google Analytics solely for the purpose of understanding
-                    user behavior and improving our website's performance. We do not sell, rent, or share this
-                    information with third parties.
+                   We don't.
                 </p>
             </div>
 
             <div className="space-y-2">
                 <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
-                    3. Cookies and Tracking Technologies
+                    2. Cookies and Tracking Technologies
                 </h2>
                 <p>
                     We use cookies and similar tracking technologies to collect and store information about your
@@ -41,7 +30,7 @@ export default function PrivacyPolicy() {
             </div>
 
             <div className="space-y-2">
-                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">4. Data Security</h2>
+                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">3. Data Security</h2>
                 <p>
                     We take reasonable measures to protect your data against unauthorized access, disclosure,
                     alteration, or destruction. However, please be aware that no method of data transmission over the
@@ -50,28 +39,8 @@ export default function PrivacyPolicy() {
             </div>
 
             <div className="space-y-2">
-                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">5. Third-Party Links</h2>
-                <p>
-                    Our website may contain links to third-party websites or services that are not operated by us. We
-                    have no control over the content, privacy policies, or practices of these third-party websites. We
-                    encourage you to review the privacy policies of these websites before providing any personal
-                    information.
-                </p>
-            </div>
-
-            <div className="space-y-2">
-                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">6. Children's Privacy</h2>
-                <p>
-                    Our website is not intended for children under the age of 13. We do not knowingly collect personal
-                    information from children. If you are a parent or guardian and believe that your child has provided
-                    us with personal information, please contact us, and we will take appropriate action to remove that
-                    information.
-                </p>
-            </div>
-
-            <div className="space-y-2">
                 <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">
-                    7. Changes to This Privacy Policy
+                    4. Changes to This Privacy Policy
                 </h2>
                 <p>
                     We reserve the right to update or modify this Privacy Policy at any time without prior notice. Any
@@ -81,10 +50,10 @@ export default function PrivacyPolicy() {
             </div>
 
             <div className="space-y-2">
-                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">8. Contact Us</h2>
+                <h2 className="text-xl md:text-2xl font-medium text-muted-foreground">5. Contact Us</h2>
                 <p>
                     If you have any questions or concerns about this Privacy Policy or the data we collect, please
-                    contact us at souhailbenlhachemi1999@gmail.com.
+                    contact us at erickbarcena00@gmail.com.
                 </p>
             </div>
 
